c98b948b9d0a85685afdb274676f8b8ecf9d77b1
Ashley Rodriguez <rodash87@gmail.com>
2022-06-06 11:49:40 -0700

    updated JS neg on local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,6 @@
 var saveBtn = $(".saveBtn")
+console.log(saveBtn)
+
 
 //Adds current day and time
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
@@ -6,9 +8,10 @@ $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 var currentTime = moment().hours();
 var description = $(".description")
 
+
 //sets color for present, past or furture timeblocks
 description.each(function () {
-    var timeBlock = parseInt($(this).attr("id"));
+    var timeBlock = parseInt($(this).attr("data-hour"));
 
     if (timeBlock === currentTime) {
         $(this).addClass("present");
@@ -27,34 +30,24 @@ description.each(function () {
     }
 });
 
+// $(document).ready(function() {
+//     var hourSlots = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
+//     $(".saveBtn").on("click", function (event) {
+//         var textTime = 
+//         var textPlans = $("#" + textTime).val();
+//         console.log(textTime)
+//         console.log(textPlans)
+    
+//         localStorage.setItem(textTime, textPlans);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//         function savePlans() {
+//             for (let i = 0; i <= hourSlots.length; i++) {
+//                 $("#" + hourSlots[i]).val(localStorage.getItem(hourSlots[i]));
+//               }
+//             }
+//             savePlans();
+//     });
+// });
 
 
